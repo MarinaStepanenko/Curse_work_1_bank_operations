cac049c1bad1b0c7cabc2ce7803a371e6f9eec16
stepanenko <m4liwka007@yandex.ru>
2025-11-13 18:30:54 +0300

    добавление функции кэшбэка

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from src.services import analyze_cashback
 from src.views import main_page
 from pprint import pprint
 
 if __name__ == "__main__":
     date_user = "2020-06-03 12:30:30"
     pprint(main_page(date_user))
+
+    print(analyze_cashback("../data/operations.xlsx", 2018, 5))
